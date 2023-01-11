88cf521edf15b3a68e56a709a50c5fc67f692e94
owaisali01 <owais.ali9091@gmail.com>
2023-01-11 23:34:38 +0500

    initial commit

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,2 @@
+class DashboardController < ActionController::Base
+end
---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,49 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :body)
+    end
+end
